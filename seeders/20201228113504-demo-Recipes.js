563a5666fa8e22af3ee5d5b7fc5f693a2cbae47d
psy-g <psykyg@gmail.com>
2020-12-30 13:52:41 +0900

    MODIFY feature/9/generate_dummy_data

--- seeders/20201228113504-demo-Recipes.js
+++ seeders/20201228113504-demo-Recipes.js
@@ -66,8 +66,17 @@ module.exports = {
           "https://recipe1.ezmember.co.kr/cache/recipe/2016/01/28/3d80ea535856cd83b9261c564a05ac0a1_m.jpg",
         title:
           "고추기름이 포인트! '순두부찌개'로 오늘 저녁 어떠세요?! ㅣ 백종원의 백종원 레시피",
-        videoId: "https://www.youtube.com/watch?v=GP8_7D5eM6A",
+        videoId: "GP8_7D5eM6A",
         // url: "https://www.10000recipe.com/recipe/6842257",
+      },
+      {
+        name: "닭갈비",
+        thumbnail:
+          "https://recipe1.ezmember.co.kr/cache/recipe/2019/10/30/33398b219faa7448ed4130b8b70e18c01_m.jpg",
+        title:
+          "초:초:초간단 만능양념장으로 만드는 춘천식 닭갈비! 1:1:1:1:1:1 ㅣ 백종원의 쿠킹로그",
+        videoId: "HI8DlDs1gko",
+        // url: "https://www.10000recipe.com/recipe/6921415",
       },
     ]);
   },
