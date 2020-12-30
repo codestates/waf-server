afcab8351a47f327bcdeb7a0ee90c650e968be6b
psy-g <psykyg@gmail.com>
2020-12-30 15:38:56 +0900

    MODIFY Feature/32/implement_getRecipe_and_playRecipe

--- controller/recipes/index.js
+++ controller/recipes/index.js
@@ -4,37 +4,33 @@ const Op = sequelize.Op;
 module.exports = {
   get: {
     getRecipes: async (req, res) => {
-      // 클라이언트 레시피 검색 api 요청 불발 시, 우리 DB에서 검색 후 데이터 반환
-      // const { item } = req.body;
-      if (req.body.item) {
-        await Recipe.findAll({
-          where: {
-            // name: { [Op.like]: `%${item}%` },
-            name: { [Op.like]: `%` },
-          },
-        }).then((data) => {
-          res.status(200).send({ recipes: data });
-        });
-      } else {
-        res.status(404).send("Not Found");
-      }
+      // 클라이언트 레시피 검색 api 요청 불발 시, 레시피 목록 반환
+
+      await Recipe.findAll({
+        where: {
+          name: { [Op.like]: `%` },
+        },
+      }).then((data) => {
+        res.status(200).send({ recipes: data });
+      });
     },
 
     playRecipe: async (req, res) => {
+      // 선택한 레시피 요청 시, 검색 후 영상 주소 반환
       const { name } = req.body;
 
-      if (name) {
+      if (!name) {
+        res.status(404).send("Not Found");
+      } else {
         await Recipe.findOne({
           where: {
             name: name,
           },
         }).then((data) => {
-          if (data) {
-            res.status(200).send({ youtubeUrl: data.url });
-          }
+          res.status(200).send({
+            youtubeUrl: `https://www.youtube.com/watch?v=${data.videoId}`,
+          });
         });
-      } else {
-        res.status(404).send("Not Found");
       }
     },
   },
